41f2c5591ce5c96899ca1272eaf7e91942e9f7d7
jfabovilla <jfabovilla@educacion.navarra.es>
2025-12-19 08:54:07 +0100

    Constructor añadido

--- Examen_Javier/src/Examen/Vehiculo.java
+++ Examen_Javier/src/Examen/Vehiculo.java
@@ -1,5 +1,16 @@
 package Examen;
 
 public class Vehiculo {
+	private String matricula, marca, modelo;
+	private double precioHora;
+
+	public Vehiculo(String matricula, String marca, String modelo, double precioHora) {
+		super();
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.precioHora = precioHora;
+	}
+
 
 }
